15f69cc4ec4702b61e7d98bdf582af128c6ccb4a
Miriam Hosak <miriam@jtwerner.net>
2013-11-26 15:32:26 +0100

    First commit

---
+++ Fuchimi-Game/src/FuchimiGame.java
@@ -0,0 +1,13 @@
+
+public class FuchimiGame {
+
+	/**
+	 * @author Miriam Hosak
+	 * @since 26.11.2013
+	 * @version 1.0
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Fuchimi");
+	}
+
+}
